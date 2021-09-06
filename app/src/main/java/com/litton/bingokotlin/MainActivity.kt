23ca95d5fa5365bc16e533848730b756735c5f5a
DannyLien <danny.lien1109@gmail.com>
2021-09-06 13:11:11 +0800

    1-7 Firebase login

--- app/src/main/java/com/litton/bingokotlin/MainActivity.kt
+++ app/src/main/java/com/litton/bingokotlin/MainActivity.kt
@@ -2,22 +2,46 @@ package com.litton.bingokotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), FirebaseAuth.AuthStateListener {
+    companion object {
+        val TAG = MainActivity::class.java.simpleName
+        val RC_SIGN_IN = 100
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 
 
     }
 
     override fun onStart() {
         super.onStart()
+        FirebaseAuth.getInstance().addAuthStateListener(this)
     }
 
     override fun onStop() {
         super.onStop()
+        FirebaseAuth.getInstance().removeAuthStateListener(this)
+    }
+
+    override fun onAuthStateChanged(auth: FirebaseAuth) {
+        if (auth.currentUser == null) {
+            startActivityForResult(
+                AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(
+                    Arrays.asList(
+                        AuthUI.IdpConfig.EmailBuilder().build(),
+                        AuthUI.IdpConfig.GoogleBuilder().build()
+                    )
+                ).build(), RC_SIGN_IN)
+        } else {
+            Log.d(TAG, "${auth.currentUser?.uid}")
+        }
     }
 
 }
